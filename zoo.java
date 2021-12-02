947563cd1334bd7cdcee1c5ecd9cd709804b27d7
Ensar Dedeoglu <76602960+ensardedeoglu@users.noreply.github.com>
2021-12-02 10:19:20 -0500

    Create zoo.java

---
+++ zoo.java
@@ -0,0 +1,17 @@
+public interface dog {
+    public String getAnimalType();
+    public int getIdTag();
+    public void setIdTag(int anIdTag);
+    public int getMinTemperature();
+    public int getMaxTemperature();
+};
+
+public void getAnimalType() {
+      System.out.println("The type of animal is 'Dog'");
+   }
+};
+
+ public void setIdTag(){
+       int anIdTag = 12;
+    };
+    
